d9ef1fc2555c7bcc9a9156256869fccbada5cd26
Will <mihaluo@126.com>
2016-03-16 19:01:49 +0800

    add server

---
+++ marchmessagequeue/MarchMessageQueueServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Hangfire;
+using MarchMessageQueue.Common;
+using MarchMessageQueue.Consumer;
+using MarchMessageQueue.Hangfire.Redis.Storage;
+using MarchMessageQueue.Subscriber;
+
+namespace MarchMessageQueue
+{
+    public static class MarchMessageQueueServer
+    {
+        public static void Start()
+        {
+            RedisStorage redisStorage = new RedisStorage("210.14.146.56:6379");
+            GlobalConfiguration.Configuration.UseStorage(redisStorage);
+            var backgroundJobServer = new BackgroundJobServer();
+
+            Type[] consumeTypes = TypeHelper.GetTypesByInterfaceType(typeof(IConsume<>));
+            ISubscriber generalSubscriber = new GeneralSubscriber();
+            ISubscriber retrySubscriber = new RetrySubscriber();
+
+            foreach (var consumeType in consumeTypes)
+            {
+                generalSubscriber.Subscribe(consumeType);
+                retrySubscriber.Subscribe(consumeType);
+            }
+
+        }
+    }
+}
